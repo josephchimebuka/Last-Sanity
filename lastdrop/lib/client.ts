f3ed971e0e152209544bf5eb9b463d6eeadd3b78
josephchimebuka <chimebujoseph@gmail.com>
2024-02-17 17:02:29 +0000

    added backend and assets

---
+++ lastdrop/lib/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@sanity/client";
+import { ImageUrlBuilder } from "sanity";
+
+export const client = createClient({
+    projectId: 'yi9r5xl6',
+    dataset: 'production',
+    useCdn: true, // Set to false if you want to fetch data from the source, not the CDN
+    apiVersion: '2022-03-07',
+    token: process.env.NEXT_PUBLIC_TOKEN
+});
--- lastdrop/src/app/page.tsx
+++ lastdrop/src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import React from 'react'
+import { client } from '../../lib/client'
+import { Category } from '../../lib/types';
 
-export default function Home() {
+
+
+async function getData(){
+  const query =`*[_type == "category"] | order(_createdAt asc) {
+    name,
+    foods[]-> {
+      name,
+      price
+    }
+  }
+`
+
+
+const data = await client.fetch(query)
+
+
+return data;
+}
+const page = async() => {
+
+
+  const data:Category[] = await getData()
+  console.log(data)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Lastdrop frontend</h1> 
-    </main>
-  );
+    <h1>Lastdrop frontend</h1> 
+   </main>
+  )
 }
+
+export default page
