7cba44930a4914c7a7ad9f506b8f9b95a7df9d06
josephchimebuka <chimebujoseph@gmail.com>
2025-06-14 11:25:50 +0100

    increased width of image

--- lastdrop/src/app/page.tsx
+++ lastdrop/src/app/page.tsx
@@ -33,7 +33,7 @@ const LandingPageHero = () => {
 
         <div className="flex flex-col md:flex-row gap-4">
           <Link href="/Outdoormenu">
-            <div className="group cursor-pointer bg-black/30 p-4 flex items-center justify-center w-[210px] h-[180px] md:w-[400px] md:h-[300px] rounded-[20px] border border-white/30 hover:border-[#FE9346] transition-all duration-300">
+            <div className="group cursor-pointer bg-black/30 p-4 flex items-center justify-center w-[220px] h-[190px] md:w-[400px] md:h-[300px] rounded-[20px] border border-white/30 hover:border-[#FE9346] transition-all duration-300">
               <Image
                 src={outdoorLogo}
                 alt="Outdoor Menu"
@@ -45,7 +45,7 @@ const LandingPageHero = () => {
           </Link>
 
           <Link href="/HideoutMenu">
-            <div className="group cursor-pointer bg-black/30 md:p-4 flex items-center justify-center w-[210px] h-[180px] md:w-[400px] md:h-[300px] rounded-[20px] border border-white/30 hover:border-[#FE9346] transition-all duration-300">
+            <div className="group cursor-pointer bg-black/30 md:p-4 flex items-center justify-center w-[220px] h-[190px] md:w-[400px] md:h-[300px] rounded-[20px] border border-white/30 hover:border-[#FE9346] transition-all duration-300">
               <Image
                 src={hideoutLogo}
                 alt="Hideout Menu"
